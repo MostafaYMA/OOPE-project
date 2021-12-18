342f43539026629e82df773dfe138fe1d7a1674e
shaymaasalah <74315271+shaymaasalah@users.noreply.github.com>
2021-12-18 17:42:07 +0200

    Create indexlogin.php

---
+++ indexlogin.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>LOGIN</title>
+  <link rel="stylesheet" type="text/css"
+        href="style.css">
+  </head>
+  <body>
+    <form action="login.php" method="post">
+      <h2>LOGIN</h2>
+      <?php if (isset($_GET['error'])) { ?>
+      <p class="error"><?php echp $_GET['error']; ?>
+      </p>
+      <?php } ?>
+      <lable>User ID</lable>
+      <input type ="text" name="UserID" placeholder="User ID"><br>
+      <lable>Password</lable>
+      <input type ="password" name="password" placeholder="password"><br>
+      <button type ="submit">Login</button>
+    </form>
+  </body>
+</html>
