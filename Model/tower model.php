f8a4eaf8bbefe611cd1d52a2e7ff5a7d52beb5c9
GentNW <89643048+GentNW@users.noreply.github.com>
2022-01-06 22:29:57 +0200

    Update tower model.php
    CRUD FUNCTIONS DONE

--- Model/tower model.php
+++ Model/tower model.php
@@ -3,31 +3,52 @@ include_once 'Database.php';
   class tower
   {
     private $db;
-    function __construct($ID,$AdminID,$Name,$Address) 
+    private $Name;
+    private $Address;
+    private $AdminID;
+    private $ye;
+    public function __construct() 
     {
         $this->db = new Database();
-        $this->conn = $this->db->connectToDB();
-        $this->ID = $ID;
-        $this->AdminID = $AdminID;
-        $this->Name = $Name;
-        $this->Address = $Address;
+        $this->ye  = $this->db->connectToDB();
     }
-    public function create()
+    public function create($AdminID,$Name,$Address)
     {
-      $stmt = $this->db->conn->prepare("INSERT INTO tower (Name,Address) VALUES (:name, :Address");
-      $this->db->conn->execute(array("name" => $this->Name, "Address" => $this->Address));
+      $this->AdminID = $AdminID;
+      $this->Name = $Name;
+      $this->Address = $Address;
+      $stmt = $this->ye->prepare("INSERT INTO `tower info`( `AdminID`, `Name`, `Address`) VALUES (?,?,?)");
+      $stmt->bind_param("iss",$this->AdminID, $this->Name, $this->Address);
+      $stmt->execute();
     }
     public function read($id)
     {
-    
+      $kms = "SELECT * FROM `tower info` WHERE `ID` = $id"; 
+      $stmt =$this->ye->query($kms);
+      if ($stmt->num_rows > 0) 
+      {
+        // output data of each row
+        while($pr = $stmt->fetch_assoc()) 
+        {
+          echo "id: " . $pr["ID"]. " - AdminID: " . $pr["AdminID"]. " - Name " . $pr["Name"]." - Address ".$pr["Address"] ."<br>";
+        }
+      } else 
+        {
+          echo "0 results";
+        }
     }
-    public function update($id)
+    public function update($id,$AdminID,$Name,$Address)
     {
-    
+      $kms = "UPDATE `tower info` SET `AdminID`=?,`Name`=?,`Address`=? WHERE ID = $id";
+      $stmt = $this->ye->prepare($kms);
+      $stmt->bind_param("iss",$AdminID, $Name, $Address);
+      $stmt->execute();
     }
     public function delete($id)
-    {
-    
+    { 
+      $kms = "DELETE FROM `tower info` WHERE ID = $id";
+      $stmt = $this->ye->prepare($kms);
+      $stmt->execute();
     }
     
   }
