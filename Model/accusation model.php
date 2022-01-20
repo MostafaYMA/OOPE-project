704f6a9c6e5680ec970d90a711e5b27d867ed2b2
GentNW <89643048+GentNW@users.noreply.github.com>
2022-01-20 20:28:42 +0200

    Update accusation model.php

--- Model/accusation model.php
+++ Model/accusation model.php
@@ -21,20 +21,20 @@ include_once 'Database.php';
       $this->Payment = $Payment;
       $this->Receipt = $Receipt;
       $this->Collection = $Collection;
-      $stmt = $this->ye->prepare("INSERT INTO `accusation`( `Statement`, `Cash amount`, `payed from`,`receipt number`,`Total`) VALUES (?,?,?,?,?)");
-      $stmt->bind_param("siiii",$this->Name, $this->Unitid, $this->Price, $this->Quantity, $this->Total);
+      $stmt = $this->ye->prepare("INSERT INTO `accusation`( `Statement`, `Cash amount`, `payed from`, `receipt number`, `Collection`) VALUES (?,?,?,?,?)");
+      $stmt->bind_param("iisis",$this->Statement, $this->Cash, $this->Payment, $this->Receipt, $this->Collection);
       $stmt->execute();
     }
     public function read($id)
     {
-      $kms = "SELECT * FROM `material with measurements` WHERE `ID` = $id"; 
+      $kms = "SELECT * FROM `accusation` WHERE `ID` = $id"; 
       $stmt =$this->ye->query($kms);
       if ($stmt->num_rows > 0) 
       {
         // output data of each row
         while($pr = $stmt->fetch_assoc()) 
         {
-          echo "id: " . $pr["ID"]. " - Name " . $pr["Name"]." - Unitid ".$pr["Unitid"] . "Price: " . $pr["Price"].   "Quantity: " . $pr["Quantity"]. "Total: " . $pr["Total"].  "<br>";
+          echo "id: " . $pr["ID"]. " - Statement: " . $pr["Statement"]." - Cash: ".$pr["Cash"] . "Payment: " . $pr["Payment"].   "Receipt: " . $pr["Receipt"]. "Collection: " . $pr["Collection"].  "<br>";
         }
       } 
       else 
@@ -42,16 +42,16 @@ include_once 'Database.php';
           echo "0 results";
       }
     }
-    public function update($id,$Name,$Unitid,$Price,$Quantity,$Total)
+    public function update($Statement,$Cash,$Payment,$Receipt,$Collection)
     {
-      $kms = "UPDATE `material with measurements` SET `Name`=?,`Unitid`=?, `Price`=?,`Quantity`=?, `Total`=? WHERE ID = $id";
+      $kms = "UPDATE `accusation` SET `Statement`=?,`Cash amount`=?,`payed from`=?,`receipt number`=?,`Collection`=? WHERE ID = $id";
       $stmt = $this->ye->prepare($kms);
-      $stmt->bind_param("siiii",$this->Name, $this->Unitid, $this->Price, $this->Quantity, $this->Total);
+      $stmt->bind_param("iisis",$this->Statement, $this->Cash, $this->Payment, $this->Receipt, $this->Collection);
       $stmt->execute();
     }
     public function delete($id)
     { 
-      $kms = "DELETE FROM `material with measurements` WHERE ID = $id";
+      $kms = "DELETE FROM `accusation` WHERE ID = $id";
       $stmt = $this->ye->prepare($kms);
       $stmt->execute();
     }
