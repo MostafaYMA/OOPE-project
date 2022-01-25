dff08b21bd26cdd31a9a48c3ee43fa0f2658fd85
shaymaasalah <74315271+shaymaasalah@users.noreply.github.com>
2022-01-25 09:01:54 -0600

    Update indexlogin.php

--- indexlogin.php
+++ indexlogin.php
@@ -9,7 +9,7 @@
     <form action="login.php" method="post">
       <h2>LOGIN</h2>
       <?php if (isset($_GET['error'])) { ?>
-      <p class="error"><?php echp $_GET['error']; ?>
+      <p class="error"><?php echo $_GET['error']; ?>
       </p>
       <?php } ?>
       <lable>User ID</lable>
